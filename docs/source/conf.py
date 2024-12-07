191b7f3b30e7ec151d50343568aba3f789f18abd
BFieguth <bfieguth@uoguelph.ca>
2024-12-07 10:27:28 -0500

    docs: setup the documentation website using the pydata_sphinx_theme

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+# Brisk documentation build configuration file, created using sphinx-quickstart
+# on December 7, 2024.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import brisk
+
+project = 'Brisk'
+copyright = '2024, Braeden Fieguth'
+author = 'Braeden Fieguth'
+version = brisk.__version__
+release = version
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx_design",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_css_files = [
+    "css/brisk.css",
+]
+
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
+html_theme_options = {
+    "show_prev_next": False,
+    "navbar_center": ["brisk_navbar.html"],
+    "navbar_end": ["theme-switcher", "version-switcher", "brisk_icon_links"],
+    "navbar_persistent": ["search-button"],
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,21 @@
+.. Brisk documentation master file, created by
+   sphinx-quickstart on Sat Dec  7 09:00:06 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Brisk |release| documentation
+=============================
+
+This is the documentation for Brisk, a Python package for training machine learning models.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   getting_started/index
+   users/index
+   api/index
+   dev/contribute
+   dev/releases
+   users/glossary
