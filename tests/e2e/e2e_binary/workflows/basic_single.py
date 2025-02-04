5515b3bd536979e45a4ad5da7a56c8be02613e3a
BFieguth <bfieguth@uoguelph.ca>
2025-02-04 16:40:35 -0500

    test: setup brisk project for binary classification

---
+++ tests/e2e/e2e_binary/workflows/basic_single.py
@@ -0,0 +1,14 @@
+from brisk.training.workflow import Workflow
+
+class BinarySingle(Workflow):
+    def workflow(self):
+        metrics = ["recall", "precision", "roc_auc"]
+        model = self.model.fit(self.X_train, self.y_train)
+        self.save_model(model, "fitted_model")
+        self.plot_roc_curve(model, self.X_test, self.y_test, "roc_curve")
+        self.plot_precision_recall_curve(
+            model, self.X_test, self.y_test, "precision_recall"
+        )
+        self.evaluate_model(
+            model, self.X_test, self.y_test, metrics, "eval_model"
+        )
