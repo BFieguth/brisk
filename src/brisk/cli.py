d1b6f4dc027f9485626ab67d0412c387af4e4225
BFieguth <bfieguth@uoguelph.ca>
2025-02-18 13:22:22 -0500

    refactor(CLI): remove n_splits as a default argument when creating as it is not used by defualt splitter

--- src/brisk/cli.py
+++ src/brisk/cli.py
@@ -110,8 +110,7 @@ METRIC_CONFIG = brisk.MetricManager(
 from brisk.data.data_manager import DataManager                
 
 BASE_DATA_MANAGER = DataManager(
-    test_size = 0.2,
-    n_splits = 5
+    test_size = 0.2
 )              
 """)
 
