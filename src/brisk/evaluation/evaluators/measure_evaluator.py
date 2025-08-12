3a288314a15ebc413e5becdf7350cc308d090757
BFieguth <bfieguth@uoguelph.ca>
2025-08-12 07:34:32 -0400

    docs(MeasureEvaluator): lint and document

--- src/brisk/evaluation/evaluators/measure_evaluator.py
+++ src/brisk/evaluation/evaluators/measure_evaluator.py
@@ -19,7 +19,26 @@ class MeasureEvaluator(BaseEvaluator):
         metrics: List[str],
         filename: str
     ) -> Dict[str, Any]:
-        """Template for all measure methods to follow."""
+        """Template for all measure methods to follow.
+        
+        Parameters
+        ----------
+        model : base.BaseEstimator
+            The model to evaluate
+        X : pd.DataFrame
+            The training data
+        y : pd.Series
+            The training labels
+        metrics : List[str]
+            The metrics to calculate
+        filename : str
+            The name of the file to save the results to
+
+        Returns
+        -------
+        Dict[str, Any]
+            The results of the evaluation
+        """
         predictions = self._generate_prediction(model, X)
         results = self._calculate_measures(predictions, y, metrics)
         metadata = self._generate_metadata(model, X.attrs["is_test"])
@@ -32,7 +51,22 @@ class MeasureEvaluator(BaseEvaluator):
         filename: str,
         metadata: Dict[str, Any]
     ) -> str:
-        """ENFORCED: Save JSON with metadata."""
+        """ENFORCED: Save JSON with metadata.
+
+        Parameters
+        ----------
+        data : Dict[str, Any]
+            The data to save
+        filename : str
+            The name of the file to save the results to
+        metadata : Dict[str, Any]
+            The metadata for the results file
+
+        Returns
+        -------
+        str
+            The path to the saved file
+        """
         output_path = self.services.io.output_dir / f"{filename}.json"
         self.io.save_to_json(data, output_path, metadata)
         return str(output_path)
@@ -42,7 +76,20 @@ class MeasureEvaluator(BaseEvaluator):
         model: base.BaseEstimator,
         X: pd.DataFrame # pylint: disable=C0103
     ) -> pd.Series:
-        """Default prediction generation - can be overridden."""
+        """Default prediction generation - can be overridden.
+
+        Parameters
+        ----------
+        model : base.BaseEstimator
+            The model to evaluate
+        X : pd.DataFrame
+            The training data
+
+        Returns
+        -------
+        pd.Series
+            The model predictions
+        """
         return model.predict(X)
 
     @abstractmethod
@@ -52,25 +99,64 @@ class MeasureEvaluator(BaseEvaluator):
         y_true: pd.Series,
         metrics: List[str]
     ) -> Dict[str, float]:
-        """Must implement this method to calculate something."""
+        """Must implement this method to calculate something.
+        
+        Parameters
+        ----------
+        predictions : pd.Series
+            The model predictions
+        y_true : pd.Series
+            The true labels
+        metrics : List[str]
+            The metrics to calculate
+
+        Returns
+        -------
+        Dict[str, float]
+            The results of the evaluation
+        """
         pass
 
-    # @abstractmethod
-    def report(self, results: Dict[str, Any]) -> Tuple[List[str], List[List[Any]]]:
+    def report(
+        self,
+        results: Dict[str, Any]
+    ) -> Tuple[List[str], List[List[Any]]]:
         """Default reporting - can be overridden.
         
         By default reporting assumes that the keys of dict are column headers
         and values are lists corresponding to each row. If the results are in a
         different format, this method should be overridden to return the list of
         column headers and a nested list of rows.
+
+        Parameters
+        ----------
+        results : Dict[str, Any]
+            The results of the evaluation
+
+        Returns
+        -------
+        Tuple[List[str], List[List[Any]]]
+            The list of column headers and a nested list of rows
         """
         columns = [key for key in results.keys() if key != "_metadata"]
         rows = [row for key in columns for row in results[key]]
         return columns, rows
-        # pass
 
     def _log_results(self, results: Dict[str, float], filename: str):
-        """Default logging - can be overridden."""
+        """Default logging - can be overridden.
+
+        Parameters
+        ----------
+        results : Dict[str, float]
+            The results of the evaluation
+        filename : str
+            The name of the file to save the results to
+
+        Returns
+        -------
+        Tuple[List[str], List[List[Any]]]
+            The list of column headers and a nested list of rows
+        """
         scores_log = "\n".join([f"{k}: {v:.4f}" for k, v in results.items()])
         self.services.logger.logger.info(
             f"Results:\n{scores_log}\n Saved to '{filename}.json'."
