345f6c4053c3c6fd4d0859475758ca3148c9c9a4
BFieguth <bfieguth@uoguelph.ca>
2025-08-27 14:28:13 -0400

    cleanup public API

--- src/brisk/__init__.py
+++ src/brisk/__init__.py
@@ -18,17 +18,20 @@ Usage:
 
 For more information, please refer to the documentation or the README file.
 """
+from brisk.configuration.algorithm_wrapper import AlgorithmWrapper, AlgorithmCollection
+from brisk.configuration.configuration_manager import ConfigurationManager
+from brisk.configuration.configuration import Configuration
+from brisk.data.data_manager import DataManager
 from brisk.defaults.regression_algorithms import REGRESSION_ALGORITHMS
 from brisk.defaults.regression_metrics import REGRESSION_METRICS
 from brisk.defaults.classification_algorithms import CLASSIFICATION_ALGORITHMS
 from brisk.defaults.classification_metrics import CLASSIFICATION_METRICS
-from brisk.data.data_manager import DataManager
 from brisk.evaluation.metric_manager import MetricManager
 from brisk.evaluation.metric_wrapper import MetricWrapper
 from brisk.evaluation.evaluation_manager import EvaluationManager
-from brisk.training.training_manager import TrainingManager
-from brisk.training.workflow import Workflow
-from brisk.configuration.algorithm_wrapper import AlgorithmWrapper, AlgorithmCollection
 from brisk.evaluation.evaluators.plot_evaluator import PlotEvaluator
 from brisk.evaluation.evaluators.measure_evaluator import MeasureEvaluator
+from brisk.evaluation.evaluators.registry import EvaluatorRegistry
+from brisk.training.training_manager import TrainingManager
+from brisk.training.workflow import Workflow
 from brisk.version import __version__
